a71568fcec2b351cd57875a29650276297a06c67
roluisker <roluisker@gmail.com>
2015-09-01 09:10:20 -0500

    homepage

---
+++ app/src/main/java/com/southwest/southwestapp/fragments/HomePageFragment.java
@@ -0,0 +1,8 @@
+package com.southwest.southwestapp.fragments;
+
+/**
+ * Created by luisalfonsobejaranosanchez on 9/1/15.
+ */
+public class HomePageFragment {
+
+}
---
+++ app/src/main/java/com/southwest/southwestapp/utils/CascadeAnimator.java
@@ -0,0 +1,8 @@
+package com.southwest.southwestapp.utils;
+
+/**
+ * Created by luisalfonsobejaranosanchez on 9/1/15.
+ */
+public class CascadeAnimator {
+
+}
