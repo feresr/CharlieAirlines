3f2bc3a18f56e22f3983b5d7c08062080aeec3c9
roluisker <roluisker@gmail.com>
2015-09-01 09:11:39 -0500

    homepage

--- app/src/main/java/com/southwest/southwestapp/fragments/HomePageFragment.java
+++ app/src/main/java/com/southwest/southwestapp/fragments/HomePageFragment.java
@@ -1,8 +1,77 @@
 package com.southwest.southwestapp.fragments;
 
+
+import com.southwest.southwestapp.R;
+import com.southwest.southwestapp.utils.CascadeAnimator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 /**
  * Created by luisalfonsobejaranosanchez on 9/1/15.
  */
-public class HomePageFragment {
+public class HomePageFragment extends Fragment{
+
+    private View mDiscountContainer;
+    private RelativeLayout mPreferredContainer;
+    private RelativeLayout mBookManageContainer;
+    private ImageView mList;
+
+    public HomePageFragment(){}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_homepage, container, false);
+
+        mDiscountContainer = view.findViewById(R.id.homepageDiscountContainer);
+        mPreferredContainer = (RelativeLayout) view.findViewById(R.id.homepagePreferredContainer);
+        mBookManageContainer = (RelativeLayout) view.findViewById(R.id.homepageBookManageContainer);
+        mList = (ImageView) view.findViewById(R.id.viewDummy);
+
+        return view;
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        List<View> viewsToAnimate = new ArrayList<View>();
+        viewsToAnimate.add(mList);
+        viewsToAnimate.add(mBookManageContainer);
+        viewsToAnimate.add(mPreferredContainer);
+        viewsToAnimate.add(mDiscountContainer);
+
+        Animation mListAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in_bottom);
+
+        CascadeAnimator animator = new CascadeAnimator(viewsToAnimate,mListAnimation);
+        animator.startCascadeAnimation();
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
 }
+
--- app/src/main/java/com/southwest/southwestapp/utils/CascadeAnimator.java
+++ app/src/main/java/com/southwest/southwestapp/utils/CascadeAnimator.java
@@ -1,8 +1,43 @@
 package com.southwest.southwestapp.utils;
 
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import java.util.List;
+
 /**
  * Created by luisalfonsobejaranosanchez on 9/1/15.
  */
 public class CascadeAnimator {
+
+    private List<View> mViewsToAnimate;
+    private Animation  mAnimation;
+    private AnimationSet as;
+
+
+    public CascadeAnimator(List<View> viewsToAnimate, Animation animation){
+        this.mViewsToAnimate = viewsToAnimate;
+        this.mAnimation = animation;
+
+        as = new AnimationSet(true);
+        as.setFillEnabled(true);
+        as.addAnimation(mAnimation);
+
+    }
+
+    public void startCascadeAnimation(){
+
+        if(mAnimation != null &&  mViewsToAnimate != null && mViewsToAnimate.size() > 0){
+
+            for(int a=0; a<mViewsToAnimate.size(); a++){
+                View view = mViewsToAnimate.get(a);
+                view.startAnimation(mAnimation);
+            }
+
+        }
+
+    }
+
 
 }
