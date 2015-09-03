808b10eb58320f18469b6d06f816780a88b6bb12
feresr <fernandoraviola@gmail.com>
2015-09-03 09:10:45 -0300

    Revert "check in confirmation advance"
    This reverts commit 0430e08d4181ec9e2dbf1740fd8387226fc679de.

--- app/src/main/java/com/southwest/southwestapp/fragments/CheckInFragment.java
+++ app/src/main/java/com/southwest/southwestapp/fragments/CheckInFragment.java
@@ -1,6 +1,5 @@
 package com.southwest.southwestapp.fragments;
 
-import com.southwest.southwestapp.AppHelper;
 import com.southwest.southwestapp.R;
 
 import android.os.Bundle;
@@ -8,15 +7,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
 /**
  * Created by emiliano.gudino on 02/09/2015.
  */
-public class CheckInFragment extends BaseFragment implements View.OnClickListener{
-
-    private Button checkIn;
+public class CheckInFragment extends BaseFragment{
 
     public CheckInFragment(){
     }
@@ -24,27 +20,11 @@ public class CheckInFragment extends BaseFragment implements View.OnClickListene
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View viewCheckin = inflater.inflate(R.layout.fragment_checkin, container, false);
-
-        checkIn = (Button) viewCheckin.findViewById(R.id.buttonCheckIn);
-
-        checkIn.setOnClickListener(this);
-
-        return viewCheckin;
+        return inflater.inflate(R.layout.fragment_checkin, container, false);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.buttonCheckIn:
-                AppHelper.screenManager.showCheckInConfirmationScreen(getActivity());
-                break;
-
-        }
     }
 }
