10a318e6d03f5ca03e49cb7af776e5f158eab24d
feresr <fernandoraviola@gmail.com>
2015-09-03 15:21:08 -0300

    rename base activity

--- app/src/main/java/com/southwest/southwestapp/activities/BaseActivity.java
+++ app/src/main/java/com/southwest/southwestapp/activities/BaseActivity.java
@@ -3,7 +3,6 @@ package com.southwest.southwestapp.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.southwest.southwestapp.R;
 
 /**
  * Created by Fernando on 3/9/2015.
@@ -12,6 +11,5 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
