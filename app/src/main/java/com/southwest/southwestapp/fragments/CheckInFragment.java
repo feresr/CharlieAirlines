6bf0966cfed192ace2d0bf6c52f28adfcff4b2ef
armando.dominguez <armando.dominguez@globant.com>
2015-09-03 15:30:27 -0500

    Changes that include styles for confirmation check in screen.

--- app/src/main/java/com/southwest/southwestapp/fragments/CheckInFragment.java
+++ app/src/main/java/com/southwest/southwestapp/fragments/CheckInFragment.java
@@ -5,14 +5,18 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.southwest.southwestapp.AppHelper;
 import com.southwest.southwestapp.R;
 
 
 /**
  * Created by emiliano.gudino on 02/09/2015.
  */
-public class CheckInFragment extends BaseFragment {
+public class CheckInFragment extends BaseFragment implements View.OnClickListener{
+
+    private Button buttonConfirmation;
 
     public CheckInFragment() {
     }
@@ -20,11 +24,28 @@ public class CheckInFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_checkin, container, false);
+
+        View checkInView = inflater.inflate(R.layout.fragment_checkin, container, false);
+
+        buttonConfirmation = (Button) checkInView.findViewById(R.id.confirmationButton);
+
+        buttonConfirmation.setOnClickListener(this);
+
+        return checkInView;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.confirmationButton:
+                AppHelper.screenManager.showCheckInConfirmationScreen(getActivity());
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/southwest/southwestapp/utils/ScreenManager.java
+++ app/src/main/java/com/southwest/southwestapp/utils/ScreenManager.java
@@ -1,12 +1,11 @@
 package com.southwest.southwestapp.utils;
 
 import com.southwest.southwestapp.R;
-import com.southwest.southwestapp.activities.BaseActivity;
+import com.southwest.southwestapp.fragments.CheckInConfirmationFragment;
 import com.southwest.southwestapp.fragments.CheckInFragment;
 import com.southwest.southwestapp.fragments.CheckInSearchFragment;
 import com.southwest.southwestapp.fragments.homepage.HomePageFragment;
 
-import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 
@@ -16,6 +15,13 @@ import android.support.v4.app.FragmentTransaction;
  */
 public class ScreenManager {
 
+    public void showCheckInConfirmationScreen(FragmentActivity origin) {
+        FragmentTransaction ft = origin.getSupportFragmentManager().beginTransaction();
+        CheckInConfirmationFragment checkinConfirmationFragment = new CheckInConfirmationFragment();
+        ft.replace(R.id.container, checkinConfirmationFragment);
+        ft.commit();
+    }
+
     public void showCheckInScreen(FragmentActivity origin) {
         FragmentTransaction ft = origin.getSupportFragmentManager().beginTransaction();
         CheckInFragment checkinFragment = new CheckInFragment();
@@ -24,9 +30,10 @@ public class ScreenManager {
     }
 
     public void showCheckInSearchScreen(FragmentActivity origin) {
-        Intent intent = new Intent(origin, BaseActivity.class);
-        intent.putExtra(BaseActivity.FRAGMENT, CheckInSearchFragment.class);
-        origin.startActivity(intent);
+        FragmentTransaction ft = origin.getSupportFragmentManager().beginTransaction();
+        CheckInSearchFragment checkinSearchFragment = new CheckInSearchFragment();
+        ft.replace(R.id.container, checkinSearchFragment);
+        ft.commit();
     }
 
     public void showMainScreen(FragmentActivity origin) {
